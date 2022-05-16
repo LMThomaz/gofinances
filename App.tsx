6acccdac42017d937d0644f8d860d2b7155feb04
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2022-05-16 16:44:40 -0300

    Styled screen SignIn

--- App.tsx
+++ App.tsx
@@ -14,6 +14,7 @@ import 'react-native-gesture-handler';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/global/styles/theme';
 import { AppRoutes } from './src/routes/app.routes';
+import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -29,7 +30,7 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <NavigationContainer>
         <StatusBar barStyle='light-content' />
-        <AppRoutes />
+        <SignIn />
       </NavigationContainer>
     </ThemeProvider>
   );
